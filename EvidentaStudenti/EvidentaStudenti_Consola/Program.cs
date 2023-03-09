04e3feee3188ca0cbe36346e8e9367afa70e7241
Felicia Giza <felicia@usm.ro>
2023-03-09 16:37:44 +0200

    initial commit - clasa Student

---
+++ EvidentaStudenti/EvidentaStudenti_Consola/Program.cs
@@ -0,0 +1,52 @@
+﻿using LibrarieModele;
+using System;
+
+namespace EvidentaStudenti_Consola
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+            int nrStudenti = 0;
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("I. Introducere informatii student");
+                Console.WriteLine("A. Afisare studenti");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "I":
+                        int idStudent = nrStudenti + 1;
+
+                        Console.WriteLine("Introdu numele studentului {0} : ", idStudent);
+                        string nume = Console.ReadLine();
+                        Console.WriteLine("Introdu prenumele studentului {0} : ", idStudent);
+                        string prenume = Console.ReadLine();
+                        student = new Student(idStudent, nume, prenume);
+                        nrStudenti++;
+
+                        break;
+                    case "A":
+                        string infoStudent = student.Info();
+                        Console.WriteLine("Studentul {0}", infoStudent);
+
+                        break;
+                    case "X":
+
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EvidentaStudenti/LibrarieModele/Student.cs
@@ -0,0 +1,33 @@
+﻿namespace LibrarieModele
+{
+    public class Student
+    {
+        private int idStudent; //identificator unic student
+        private string nume;
+        private string prenume;
+
+        //contructor implicit
+        public Student()
+        {
+            nume = prenume = string.Empty;
+        }
+
+        //constructor cu parametri
+        public Student(int idStudent, string nume, string prenume)
+        {
+            this.idStudent = idStudent;
+            this.nume = nume;
+            this.prenume = prenume;
+        }
+
+        public string Info()
+        {
+            string info = string.Format("Id:{0} Nume:{1} Prenume: {2}",
+                idStudent.ToString(),
+                (nume ?? " NECUNOSCUT "),
+                (prenume ?? " NECUNOSCUT "));
+
+            return info;
+        }
+    }
+}
